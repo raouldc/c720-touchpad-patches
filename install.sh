d70e13d6d8fbda80dd2bbf196ec0ad9deb97d513
Raoul <raoul_dcunha@hotmail.com>
2014-02-22 10:30:57 +1300

    fixed paths in the install script

--- install.sh
+++ install.sh
@@ -30,13 +30,13 @@ make prepare
 make modules_prepare
 
 # Build only the needed directories
-make SUBDIRS=drivers/platform/x86 modules
+make SUBDIRS=drivers/platform/chrome modules
 make SUBDIRS=drivers/i2c/busses modules
 
 # switch to using our new chromeos_laptop.ko module
 # preserve old as .orig
-sudo mv /lib/modules/$mykern/kernel/drivers/platform/x86/chromeos_laptop.ko /lib/modules/$mykern/kernel/drivers/platform/x86/chromeos_laptop.ko.orig
-sudo cp drivers/platform/x86/chromeos_laptop.ko /lib/modules/$mykern/kernel/drivers/platform/x86/
+sudo mv /lib/modules/$mykern/kernel/drivers/platform/chrome/chromeos_laptop.ko /lib/modules/$mykern/kernel/drivers/platform/chrome/chromeos_laptop.ko.orig
+sudo cp drivers/platform/chrome/chromeos_laptop.ko /lib/modules/$mykern/kernel/drivers/platform/chrome/
 
 # switch to using our new designware i2c modules
 # preserve old as .orig
